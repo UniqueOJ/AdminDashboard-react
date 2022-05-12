765e645210c2276399b1a12ccf9dacdfa769bd2b
Ojochegbe Alikali <joyalikali77@gmail.com>
2022-05-12 09:47:09 +0100

    implement widgets

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,14 @@
+import './widgetSm.css'
+
+export default function widgetSm() {
+  return (
+    <div className='widgetSm'>
+      <span className="widgetSmTitle">New Members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+            <img src="" alt="" className="widgetSmImg"/>
+        </li>
+      </ul>
+    </div>
+  )
+}
