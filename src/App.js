b29b1870909c6b3db8128c8c71d0de6b8c8dff5f
Ojochegbe Alikali <joyalikali77@gmail.com>
2022-05-25 17:02:34 +0100

    Implement ProductList

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -17,11 +18,10 @@ function App() {
         <Route exact path="/" element={<Home/>}></Route>
         <Route path="/Users" element={<UserList/>}></Route>
         <Route path="/User/:userId" element={<User/>}></Route>
-        <Route path="/newUser" element={<NewUser />}></Route>
-        
-        {/* <Route path="/products" element={<ProductList />}></Route>
-        <Route path="/product/:productId" element={<Product />}></Route>
-        <Route path="/newproduct" element={<NewProduct />}></Route> */}
+        <Route path="/newUser" element={<NewUser/>}></Route>
+        <Route path="/products" element={<ProductList/>}></Route>
+        {/* <Route path="/product/:productId" element={<Product/>}></Route>
+        <Route path="/newproduct" element={<NewProduct/>}></Route> */}
      </Routes>
      </div>
   </Router>
